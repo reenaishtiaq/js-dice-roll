44c5aa886e25d219e1ce9c9f9084ccfc31c6f481
reenaishtiaq <49521474+reenaishtiaq@users.noreply.github.com>
2019-07-10 23:49:56 +0500

    Made it Responsive

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function rollDice() {
     
     
     // Math.floor means ---> roundoff the number to the nearest value which user get by rolling.
-    // Math.random means ---> return a number from 0  t0 6 randomly.
+    // Math.random means ---> return a number from 0  to 6 randomly.
     
     let fDice = Math.floor(Math.random() * 7) + 1;
     let sDice = Math.floor(Math.random() * 7) + 1;
@@ -22,13 +22,11 @@ function rollDice() {
     
     dice1.innerHTML = fDice;
     dice2.innerHTML = sDice;
-    info.innerText = "You rolled" + totalDice + ".";
+    info.innerText = "You rolled  " + totalDice + ".";
     
-    // if the value of both dices are same then user have another chnace to roll the dice.
+    // if the value of both dices are same then user have another chance to roll the dice.
     if (fDice == sDice) {
         info.innerHTML += " Waooo! you got a free chance";
     }
-
-
 }
 
